266d0bb0bf6719afda7117517e0d738d0e563740
Florent <florent@frizoncorrea.fr>
2025-08-28 08:48:51 +0200

    issue in pre_build.py

--- pre_build.py
+++ pre_build.py
@@ -65,5 +65,3 @@ if add_exampledir_to_incs:
     example_dir = f".pio/libdeps/{env_name}/MeshCore/examples/{example_name}"
     print(f"adding {example_dir} to includes")
     menv.Append(BUILD_FLAGS=[f"-I {example_dir}"])
-
-menv.Append(BUILD_FLAGS=[f"-I"])
